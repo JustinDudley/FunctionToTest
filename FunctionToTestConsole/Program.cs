46722aeab085c5c38e011966cd4e437ca076f906
Justin Dudley <justindudley99@gmail.com>
2019-09-09 14:18:31 -0400

    Add project files.

---
+++ FunctionToTestConsole/Program.cs
@@ -0,0 +1,10 @@
+﻿using System;
+using FunctionToTestLibrary;
+
+namespace FunctionToTestConsole {
+    class Program {
+        static void Main(string[] args) {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
---
+++ FunctionToTestLibrary/Class1.cs
@@ -0,0 +1,6 @@
+﻿using System;
+
+namespace FunctionToTestLibrary {
+    public class Class1 {
+    }
+}
