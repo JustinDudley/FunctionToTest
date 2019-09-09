e9ff8e326e0bf7f3537c3d4a0bdaac92ccaf150b
Justin Dudley <justindudley99@gmail.com>
2019-09-09 14:23:46 -0400

    initial commit

---
+++ FunctionToTestLibrary/ForecastingFunction.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace FunctionToTestLibrary {
+    public class ForecastingFunction {
+
+        //PROPERTY
+        public int Multiplier { get; set; } = 0;
+
+        //METHODS
+        public int Forecast(int x, int y) {
+            return ((x * x * x) + (y * y)) * Multiplier;
+        }
+    }
+}
